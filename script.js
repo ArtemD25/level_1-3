c45c9005798dfdd91c3e913a605dd35ce98d14d8
Seagull <ssean5739@gmail.com>
2021-08-02 16:34:18 +0300

    Added table styles and decomposed the code

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 function DataTable(config, data) {
+  const numOfColumns = config.columns.length + 1;
+  const table = createTableWrapperAndTable(config);
+  const tHead = createTableHead(table);
+  const tHeadTr = createTableHeadTr(tHead);
+  createTableHeadTds(tHeadTr, config, numOfColumns);
+  const tBody = createTableBody(table);
+  createBodyTrs(data, numOfColumns, tBody);
+}
+
+function createTableWrapperAndTable(config) {
   const tableWrapper = document.getElementById(`${config.parent.slice(1)}`);
   const table = document.createElement("table");
   table.classList.add("my-table");
   tableWrapper.appendChild(table);
-  const numOfColumns = config.columns.length + 1;
+  return table;
+}
 
+// Creates Header
+function createTableHead(table) {
   const tHead = document.createElement("thead");
   tHead.classList.add("my-table__header");
   table.appendChild(tHead);
+  return tHead;
+}
 
+// Creates Header tr
+function createTableHeadTr(tHead) {
   const tHeadTr = document.createElement("tr");
   tHeadTr.classList.add("my-table__header-row");
   tHead.appendChild(tHeadTr);
+  return tHeadTr;
+}
 
+// Creates Header td-s
+function createTableHeadTds(tHeadTr, config, numOfColumns) {
   for (let i = 0; i < numOfColumns; i++) {
     const tHeadTd = document.createElement("td");
     tHeadTd.classList.add("my-table__header-cell");
@@ -20,6 +41,47 @@ function DataTable(config, data) {
     tHeadTd.textContent === "№" ? tHeadTd.setAttribute("data-my-table", "number") : tHeadTd.setAttribute("data-my-table", config.columns[i - 1].value);
     tHeadTr.appendChild(tHeadTd);
   }
+}
+
+// Creates Body
+function createTableBody(table) {
+  const tBody = document.createElement("tbody");
+  tBody.classList.add("my-table__body");
+  table.appendChild(tBody);
+  return tBody;
+}
+
+// Creates Body tr-s
+function createBodyTrs(data, numOfColumns, tBody) {
+  data.forEach((item, index) => {
+    createBodyTr(numOfColumns, tBody, index, item);
+  })
+}
+
+// Creates Body tr-s
+function createBodyTr(numOfColumns, tBody, index, item) {
+  const tBodyTr = document.createElement("tr");
+  tBodyTr.classList.add("my-table__body-row");
+  createTableBodyTds(tBodyTr, numOfColumns, index, item);
+  
+  tBody.appendChild(tBodyTr);
+}
+
+// Creates Body td-s
+function createTableBodyTds(tBodyTr, numOfColumns, index, item) {
+  for (let i = 0; i < numOfColumns; i++) {
+    const tBodyTd = document.createElement("td");
+    tBodyTd.classList.add("my-table__body-cell");
+    tBodyTd.textContent = i === 0 ? index + 1 : getTextContent(i, item);
+    tBodyTr.appendChild(tBodyTd);
+  }
+}
+
+// Creates text elements for body td-s
+function getTextContent(index, item) {
+  const correspondingHeadTr = document.querySelector(`.my-table__header-row td:nth-child(${index + 1})`);
+  const key = correspondingHeadTr.getAttribute("data-my-table");
+  return (item[key]);
 }
 
 const config1 = {
